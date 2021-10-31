da7a2f203076e72bf961989c90089a4f611faf22
kristus123 <krispetter@gmail.com>
2021-10-31 15:14:01 +0100

    added comments on an alternative solution

--- alien_detector/alien/AlienDetector.py
+++ alien_detector/alien/AlienDetector.py
@@ -6,6 +6,16 @@ class AlienDetector:
 	def __init__(self, radar):
 		self.radar = radar
 
+	# Another solution could be to fetch a list of areas with a certain length and width
+	# and then compare those chunks with the alien body to see if there is a match.
+	# That would maybe be a more optimized way of solving this task, and maybe even more readable.
+	# If done that way, it would maybe look like this:
+	"""
+	def find_all(self, alien: Alien) -> (Position):
+		for area, position in self.radar.split_map_into_smaller_chunks(alien.height, alien.width):
+			if area == alien.body:
+				yield position
+	"""
 
 	def find_all(self, alien: Alien) -> (Position):
 		for p in [p.relative_to(alien.body) for p in self.radar.find_occurrences(alien.head)]:
